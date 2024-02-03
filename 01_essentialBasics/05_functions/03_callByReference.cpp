ef12eb4f1718d8c3b267ffef233b549b12a323cf
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-02-03 16:27:26 +0530

    swap function using call by reference

---
+++ 01_essentialBasics/05_functions/03_callByReference.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+void Swap(int &a,int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	int x,y;
+	x=15;
+	y=21;
+	cout<<"Values before swap: "<<x<<" "<<y<<endl;
+	Swap(x,y);
+	cout<<"Values after swap: "<<x<<" "<<y<<endl;
+
+	return 0;
+}
