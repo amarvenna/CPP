eb274675fb187d24d27a7f36795434e3fe9769b0
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-02-03 15:12:55 +0530

    pointer basics

---
+++ 01_essentialBasics/03_pointers/03_pointerBasics.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int A=10;
+	int *p; //address variable
+	p=&A;
+
+	cout<<A<<endl;
+	cout<<&A<<endl;
+	cout<<p<<endl;
+	cout<<*p<<endl;
+
+	return 0;
+}
+
+
